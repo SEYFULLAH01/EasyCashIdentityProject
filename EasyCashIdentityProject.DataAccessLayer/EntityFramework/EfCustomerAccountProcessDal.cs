8be6c62c12aa2963f05cdcc375f9822427c5f11d
SEYFULLAH01 <120310366+SEYFULLAH01@users.noreply.github.com>
2025-05-01 18:53:17 +0300

    Rapid Api ile döviz verileri çekildi ıban ve fatura işlemleri yapılacak

--- EasyCashIdentityProject.DataAccessLayer/EntityFramework/EfCustomerAccountProcessDal.cs
+++ EasyCashIdentityProject.DataAccessLayer/EntityFramework/EfCustomerAccountProcessDal.cs
@@ -1,6 +1,8 @@
 ﻿using EasyCashIdentityProject.DataAccessLayer.Abstract;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
 using EasyCashIdentityProject.DataAccessLayer.Repositories;
 using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace EasyCashIdentityProject.DataAccessLayer.EntityFramework
 {
@@ -8,7 +10,9 @@ namespace EasyCashIdentityProject.DataAccessLayer.EntityFramework
     {
         public List<CustomerAccountProcess> MyLastProcess(int id)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            var values = context.CustomerAccountProcesses.Include(y => y.SenderCustomer).Include(w => w.ReceiverCustomer).ThenInclude(z => z.AppUser).Where(x => x.ReceiverID == id || x.SenderID == id).ToList();
+            return values;
         }
     }
 }
